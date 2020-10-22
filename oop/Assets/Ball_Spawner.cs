918f7b39a09344a1c65a7ffe9060c9f3db791b0d
AidanBugeja <56017014+AidanBugeja@users.noreply.github.com>
2020-10-22 13:30:34 +0200

    Main folder

---
+++ oop/Assets/Ball_Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Ball_Spawner : MonoBehaviour
+{
+    public GameObject myCirclePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 mypos = new Vector3(7.5f, 7.5f, 0);
+        Vector3 mypos2 = new Vector3(-7.5f, -7.5f, 0);
+        Vector3 mypos3 = new Vector3(-7.5f, 7.5f, 0);
+        Vector3 mypos4 = new Vector3(7.5f, -7.5f, 0);
+       GameObject mycircle1 = Instantiate(myCirclePrefab, mypos , Quaternion.identity);
+       GameObject mycircle2 = Instantiate(myCirclePrefab, mypos2, Quaternion.identity);
+       GameObject mycircle3 = Instantiate(myCirclePrefab, mypos3, Quaternion.identity);
+       GameObject mycircle4 = Instantiate(myCirclePrefab, mypos4, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
