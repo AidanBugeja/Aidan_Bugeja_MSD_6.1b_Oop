5be19d8117ad6e397933619174ada882094c1dc3
AidanBugeja <56017014+AidanBugeja@users.noreply.github.com>
2020-10-22 15:20:45 +0200

    lesson 2

---
+++ Lecture_2/Assets/Randomspawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Randomspawner : MonoBehaviour
+{
+    public GameObject myCirclePrefab;
+    public float padding;
+    //private GameObject myCirclePrefab;
+    private float XMin, XMax, YMin, YMax;
+    private float timer = 0;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        Camera myCamera = Camera.main;
+        XMin = myCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        Debug.Log("XMin: " + XMin.ToString());
+        XMax = myCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        Debug.Log("XMax: " + XMax.ToString());
+        YMin = myCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        Debug.Log("YMin: " + YMin.ToString());
+        YMax = myCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        Debug.Log("YMax: " + YMax.ToString());
+
+
+        //myCirclePrefab = (GameObject)Resources.Load("Circle");
+
+        GameObject mycircle1 = Instantiate(myCirclePrefab, new Vector3(XMin, YMin, 0), Quaternion.identity);
+        GameObject mycircle2 = Instantiate(myCirclePrefab, new Vector3(XMin, YMax, 0), Quaternion.identity);
+        GameObject mycircle3 = Instantiate(myCirclePrefab, new Vector3(XMax, YMax, 0), Quaternion.identity);
+        GameObject mycircle4 = Instantiate(myCirclePrefab, new Vector3(XMax, YMin, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(timer);
+        timer += Time.deltaTime * 1000;
+        if ((int)(timer) % 100 == 0)
+        {
+     
+            Vector2 newposition = new Vector2(Random.Range(XMin, XMax), Random.Range(YMin, YMax));
+            Instantiate(myCirclePrefab, new Vector3(newposition.x, newposition.y, 0), Quaternion.identity);
+        }
+    }
+}
--- oop/Assets/Ball_Spawner.cs
+++ oop/Assets/Ball_Spawner.cs
@@ -6,18 +6,11 @@ using UnityEngine;
 
 public class Ball_Spawner : MonoBehaviour
 {
-    public GameObject myCirclePrefab;
+    private GameObject myCirclePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 mypos = new Vector3(7.5f, 7.5f, 0);
-        Vector3 mypos2 = new Vector3(-7.5f, -7.5f, 0);
-        Vector3 mypos3 = new Vector3(-7.5f, 7.5f, 0);
-        Vector3 mypos4 = new Vector3(7.5f, -7.5f, 0);
-       GameObject mycircle1 = Instantiate(myCirclePrefab, mypos , Quaternion.identity);
-       GameObject mycircle2 = Instantiate(myCirclePrefab, mypos2, Quaternion.identity);
-       GameObject mycircle3 = Instantiate(myCirclePrefab, mypos3, Quaternion.identity);
-       GameObject mycircle4 = Instantiate(myCirclePrefab, mypos4, Quaternion.identity);
+        myCirclePrefab = (GameObject)Resources
     }
 
     // Update is called once per frame
